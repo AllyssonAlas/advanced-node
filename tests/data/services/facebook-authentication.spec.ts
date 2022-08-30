b3a72fd2dd28695ff0a2547ba0ce2dac796c726c
Allysson <allyssonmgs@gmail.com>
2022-08-30 20:55:43 -0300

    refactor: simplify code

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -7,6 +7,7 @@ import { LoadFacebookUserApi } from '@/data/contracts/apis';
 describe('FacebookAuthenticationService', () => {
   let loadFacebookUserApi: MockProxy<LoadFacebookUserApi>;
   let sut: FacebookAuthenticationService;
+  const token = 'any_token';
 
   beforeEach(() => {
     loadFacebookUserApi = mock();
@@ -14,16 +15,16 @@ describe('FacebookAuthenticationService', () => {
   });
 
   it('Should call LoadFacebookUserApi with correct params', async () => {
-    await sut.perform({ token: 'any_token' });
+    await sut.perform({ token });
 
-    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token: 'any_token' });
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token });
     expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1);
   });
 
   it('Should return AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
     loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined);
 
-    const authResult = await sut.perform({ token: 'any_token' });
+    const authResult = await sut.perform({ token });
 
     expect(authResult).toEqual(new AuthenticationError());
   });
