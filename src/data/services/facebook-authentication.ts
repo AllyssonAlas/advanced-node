72df48df4de4a70b684c1308d5be58b62242efa8
Allysson <allyssonmgs@gmail.com>
2022-08-31 20:13:29 -0300

    feat: ensure FacebookAuthenticationService calls CreateFacebookAccountRepo with correct params

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -1,18 +1,23 @@
 import { AuthenticationError } from '@/domain/errors';
 import { FacebookAuthentication } from '@/domain/features';
 import { LoadFacebookUserApi } from '@/data/contracts/apis';
-import { LoadUserAccountRepository } from '@/data/contracts/repositories';
+import {
+  CreateFacebookAccountRepository,
+  LoadUserAccountRepository,
+} from '@/data/contracts/repositories';
 
 export class FacebookAuthenticationService {
   constructor(
     private readonly loadFacebookUserApi: LoadFacebookUserApi,
     private readonly loadUserAccountRepository: LoadUserAccountRepository,
+    private readonly createUserAccountRepository: CreateFacebookAccountRepository,
   ) {}
 
   async perform(params: FacebookAuthentication.Params): Promise<AuthenticationError> {
     const fbData = await this.loadFacebookUserApi.loadUser(params);
     if (fbData) {
       await this.loadUserAccountRepository.load({ email: fbData.email });
+      await this.createUserAccountRepository.createFromFacebook(fbData);
     }
     return new AuthenticationError();
   }
