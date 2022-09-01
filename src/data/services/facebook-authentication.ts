df03149c8d331199494e0634dc96eb3720ea7f74
Allysson <allyssonmgs@gmail.com>
2022-09-01 19:11:06 -0300

    feat:  remove implementation details from service

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -2,32 +2,27 @@ import { AuthenticationError } from '@/domain/errors';
 import { FacebookAuthentication } from '@/domain/features';
 import { LoadFacebookUserApi } from '@/data/contracts/apis';
 import {
-  CreateFacebookAccountRepository,
+  SaveFacebookAccountRepository,
   LoadUserAccountRepository,
-  UpdateFacebookAccountRepository,
 } from '@/data/contracts/repositories';
 
 export class FacebookAuthenticationService {
   constructor(
     private readonly facebookApi: LoadFacebookUserApi,
     private readonly userAccountRepository: LoadUserAccountRepository &
-      CreateFacebookAccountRepository &
-      UpdateFacebookAccountRepository,
+      SaveFacebookAccountRepository,
   ) {}
 
   async perform(params: FacebookAuthentication.Params): Promise<AuthenticationError> {
     const fbData = await this.facebookApi.loadUser(params);
     if (fbData) {
       const accountData = await this.userAccountRepository.load({ email: fbData.email });
-      if (accountData) {
-        await this.userAccountRepository.updateWithFacebook({
-          id: accountData.id,
-          name: accountData.name ?? fbData.name,
-          facebookId: fbData.facebookId,
-        });
-      } else {
-        await this.userAccountRepository.createFromFacebook(fbData);
-      }
+      await this.userAccountRepository.saveWithFacebook({
+        id: accountData?.id,
+        name: accountData?.name ?? fbData.name,
+        email: fbData.email,
+        facebookId: fbData.facebookId,
+      });
     }
     return new AuthenticationError();
   }
