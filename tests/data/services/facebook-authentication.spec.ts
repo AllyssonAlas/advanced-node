0806886e75c42079a9ecb8944f5333601ec6b3f0
Allysson <allyssonmgs@gmail.com>
2022-09-01 18:49:51 -0300

    test: ensure FacebookAuthenticationService update  account if LoadUserAccountRepository returns data

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -5,11 +5,14 @@ import { FacebookAuthenticationService } from '@/data/services';
 import {
   CreateFacebookAccountRepository,
   LoadUserAccountRepository,
+  UpdateFacebookAccountRepository,
 } from '@/data/contracts/repositories';
 import { LoadFacebookUserApi } from '@/data/contracts/apis';
 
 describe('FacebookAuthenticationService', () => {
-  let userAccountRepo: MockProxy<LoadUserAccountRepository & CreateFacebookAccountRepository>;
+  let userAccountRepo: MockProxy<
+    LoadUserAccountRepository & CreateFacebookAccountRepository & UpdateFacebookAccountRepository
+  >;
   let facebookApi: MockProxy<LoadFacebookUserApi>;
   let sut: FacebookAuthenticationService;
   const token = 'any_token';
@@ -40,14 +43,14 @@ describe('FacebookAuthenticationService', () => {
     expect(authResult).toEqual(new AuthenticationError());
   });
 
-  it('Should call UserAccountRepo when LoadFacebookUserApi returns data', async () => {
+  it('Should call LoadUserAccountRepo when LoadFacebookUserApi returns data', async () => {
     await sut.perform({ token });
 
     expect(userAccountRepo.load).toHaveBeenCalledWith({ email: 'any_fb_email' });
     expect(userAccountRepo.load).toHaveBeenCalledTimes(1);
   });
 
-  it('Should call UserAccountRepo when LoadUserAcountRepo returns undefined', async () => {
+  it('Should call CreateFacebookAccountRepo when LoadUserAcountRepo returns undefined', async () => {
     userAccountRepo.load.mockResolvedValueOnce(undefined);
 
     await sut.perform({ token });
@@ -58,5 +61,21 @@ describe('FacebookAuthenticationService', () => {
       facebookId: 'any_fb_id',
     });
     expect(userAccountRepo.createFromFacebook).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should call UpdateFacebookAccountRepo when LoadUserAcountRepo returns undefined', async () => {
+    userAccountRepo.load.mockResolvedValueOnce({
+      id: 'any_id',
+      name: 'any_name',
+    });
+
+    await sut.perform({ token });
+
+    expect(userAccountRepo.updateWithFacebook).toHaveBeenCalledWith({
+      id: 'any_id',
+      name: 'any_name',
+      facebookId: 'any_fb_id',
+    });
+    expect(userAccountRepo.updateWithFacebook).toHaveBeenCalledTimes(1);
   });
 });
