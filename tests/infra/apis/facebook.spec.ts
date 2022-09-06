7eb02e2a780b747a49984b58c4ca13f2259ddb0f
Allysson <allyssonmgs@gmail.com>
2022-09-06 20:08:47 -0300

    test: ensure FacebookApi get app token with correct url

---
+++ tests/infra/apis/facebook.spec.ts
@@ -0,0 +1,35 @@
+import { mock } from 'jest-mock-extended';
+
+import { LoadFacebookUserApi } from '@/data/contracts/apis';
+
+interface HttpGetClient {
+  get: (params: HttpGetClient.Params) => Promise<void>;
+}
+
+namespace HttpGetClient {
+  export type Params = {
+    url: string;
+  };
+}
+
+class FacebookApi {
+  private readonly baseUrl = 'https://graph.facebook.com';
+  constructor(private readonly httpClient: HttpGetClient) {}
+
+  async loadUser(params: LoadFacebookUserApi.Params): Promise<void> {
+    await this.httpClient.get({ url: `${this.baseUrl}/oauth/access_token` });
+  }
+}
+
+describe('FacebookApi', () => {
+  it('Should get app token', async () => {
+    const httpClient = mock<HttpGetClient>();
+    const sut = new FacebookApi(httpClient);
+
+    await sut.loadUser({ token: 'any_client_token' });
+
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/oauth/access_token',
+    });
+  });
+});
