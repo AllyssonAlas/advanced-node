0ef28595780112abb30b0eb99bc561b4250551fa
Allysson <allyssonmgs@gmail.com>
2022-09-07 20:20:55 -0300

    chore: remove index files and main layer from coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   clearMocks: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/index.ts',
+  ],
   coverageDirectory: "coverage",
   coverageProvider: "babel",
   moduleNameMapper: {
