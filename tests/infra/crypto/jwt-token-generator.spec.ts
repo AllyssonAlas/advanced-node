00ececb7e0791a20fada9f83ca426e8e713dbc55
Allysson <allyssonmgs@gmail.com>
2022-09-08 19:51:27 -0300

    refactor: isolate sut creation

--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -14,12 +14,22 @@ class JwtTokenGenerator {
 }
 
 describe('JwtTokenGenerator', () => {
-  it('Should call sign with correct params', async () => {
-    const fakeJwt = jwt as jest.Mocked<typeof jwt>;
-    const sut = new JwtTokenGenerator('any_secret');
+  let sut: JwtTokenGenerator;
+  let fakeJwt: jest.Mocked<typeof jwt>;
+  let secret: string;
 
+  beforeAll(() => {
+    fakeJwt = jwt as jest.Mocked<typeof jwt>;
+    secret = 'any_secret';
+  });
+
+  beforeEach(() => {
+    sut = new JwtTokenGenerator(secret);
+  });
+
+  it('Should call sign with correct params', async () => {
     await sut.generateToken({ key: 'any_key', expirationInMs: 1000 });
 
-    expect(fakeJwt.sign).toHaveBeenCalledWith({ key: 'any_key' }, 'any_secret', { expiresIn: 1 });
+    expect(fakeJwt.sign).toHaveBeenCalledWith({ key: 'any_key' }, secret, { expiresIn: 1 });
   });
 });
