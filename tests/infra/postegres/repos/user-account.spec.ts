64a171cef75544a9e710ce963637e04d2b96613e
Allysson <allyssonmgs@gmail.com>
2022-09-13 20:37:25 -0300

    refactor: move sut creation to main describe

--- tests/infra/postegres/repos/user-account.spec.ts
+++ tests/infra/postegres/repos/user-account.spec.ts
@@ -7,26 +7,26 @@ import { PgUser } from '@/infra/postgres/entities';
 import { makeFakeDb } from '@/tests/infra/postegres/mocks';
 
 describe('PgUserAccountRepository', () => {
+  let sut: PgUserAccountRepository;
+  let pgUserRepo: Repository<PgUser>;
+  let backup: IBackup;
+
+  beforeAll(async () => {
+    const db = await makeFakeDb([PgUser]);
+    backup = db.backup();
+    pgUserRepo = getRepository(PgUser);
+  });
+
+  beforeEach(() => {
+    backup.restore();
+    sut = new PgUserAccountRepository();
+  });
+
+  afterAll(async () => {
+    await getConnection().close();
+  });
+
   describe('load', () => {
-    let sut: PgUserAccountRepository;
-    let pgUserRepo: Repository<PgUser>;
-    let backup: IBackup;
-
-    beforeAll(async () => {
-      const db = await makeFakeDb([PgUser]);
-      backup = db.backup();
-      pgUserRepo = getRepository(PgUser);
-    });
-
-    beforeEach(() => {
-      backup.restore();
-      sut = new PgUserAccountRepository();
-    });
-
-    afterAll(async () => {
-      await getConnection().close();
-    });
-
     it('Should return an account if email exists', async () => {
       await pgUserRepo.save({ email: 'existing_email' });
 
