874821be1e8aff6d280e9baf502c4a4ebaff6345
Allysson <allyssonmgs@gmail.com>
2022-09-15 19:34:34 -0300

    test: ensure RequiredStringValidator if value is empty

---
+++ tests/application/validation/required-string.spec.ts
@@ -0,0 +1,28 @@
+import { RequiredStringValidator } from '@/application/validation';
+import { RequiredFieldError } from '@/application/errors';
+
+describe('RequiredStringValidator', () => {
+  it('Should return RequiredFieldError if value is empty', () => {
+    const sut = new RequiredStringValidator('', 'any_field');
+
+    const error = sut.validate();
+
+    expect(error).toEqual(new RequiredFieldError('any_field'));
+  });
+
+  it('Should return RequiredFieldError if value is null', () => {
+    const sut = new RequiredStringValidator(null as any, 'any_field');
+
+    const error = sut.validate();
+
+    expect(error).toEqual(new RequiredFieldError('any_field'));
+  });
+
+  it('Should return RequiredFieldError if value is undefined', () => {
+    const sut = new RequiredStringValidator(undefined as any, 'any_field');
+
+    const error = sut.validate();
+
+    expect(error).toEqual(new RequiredFieldError('any_field'));
+  });
+});
