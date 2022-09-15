8653e7c60140c64b27caa59c0b235e447eaa62a5
Allysson <allyssonmgs@gmail.com>
2022-09-14 21:16:56 -0300

    test: ensure FacebookLoginController returns 400 if token is null

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -11,4 +11,15 @@ describe('FacebookLoginController', () => {
       data: new Error('the field token is required'),
     });
   });
+
+  it('Should return 400 if token is null', async () => {
+    const sut = new FacebookLoginController();
+
+    const httpResponse = await sut.handle({ token: null });
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new Error('the field token is required'),
+    });
+  });
 });
