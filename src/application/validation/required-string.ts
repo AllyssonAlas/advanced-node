7b041108213cbd755b3c45ce0d1267d2a2fcfcb5
Allysson <allyssonmgs@gmail.com>
2022-09-15 19:38:41 -0300

    feat: ensure RequiredStringValidator returns undefined is value is valid

--- src/application/validation/required-string.ts
+++ src/application/validation/required-string.ts
@@ -4,6 +4,8 @@ export class RequiredStringValidator {
   constructor(private readonly value: string, private readonly fieldName: string) {}
 
   validate(): Error | undefined {
-    return new RequiredFieldError('any_field');
+    if (!this.value) {
+      return new RequiredFieldError(this.fieldName);
+    }
   }
 }
