71e35ed676d6e03a8c627b4a215492245b8d338d
Allysson <allyssonmgs@gmail.com>
2022-09-17 19:11:26 -0300

    test: ensure ValidationComposite returns undefined if all validators return undefined

---
+++ tests/application/validation/validation-composite.spec.ts
@@ -0,0 +1,18 @@
+import { mock } from 'jest-mock-extended';
+
+import { ValidationComposite, Validator } from '@/application/validation';
+
+describe('ValidationComposite', () => {
+  it('Should return undefined if all  validators return undefined', () => {
+    const validator1 = mock<Validator>();
+    validator1.validate.mockReturnValue(undefined);
+    const validator2 = mock<Validator>();
+    validator2.validate.mockReturnValue(undefined);
+    const validators = [validator1, validator2];
+    const sut = new ValidationComposite(validators);
+
+    const error = sut.validate();
+
+    expect(error).toBeUndefined();
+  });
+});
