5406ec0eb4f47230bf38d03c86e9d9287e081a20
Allysson <allyssonmgs@gmail.com>
2022-09-20 13:34:43 -0300

    refactor: isolate sut creation

--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -2,14 +2,15 @@ import { AxiosHttpClient, FacebookApi } from '@/infra/apis';
 import { env } from '@/main/config/env';
 
 describe('Facebook Api Integration Tests', () => {
-  it('Should return a Facebook User if token is valid', async () => {
-    const axiosClient = new AxiosHttpClient();
-    const sut = new FacebookApi(
-      axiosClient,
-      env.facebookApi.clientId,
-      env.facebookApi.clientSecret,
-    );
+  let axiosClient: AxiosHttpClient;
+  let sut: FacebookApi;
 
+  beforeEach(() => {
+    axiosClient = new AxiosHttpClient();
+    sut = new FacebookApi(axiosClient, env.facebookApi.clientId, env.facebookApi.clientSecret);
+  });
+
+  it('Should return a Facebook User if token is valid', async () => {
     const fbUser = await sut.loadUser({
       token:
         'EAAYcfMTjiLMBAMgS6IW1ECAjYUBvZCVEeNrBa0E8q5BJDMkrOeVflrxIuPvUEvtKP8tBL6617k0o9AfRWXzL0zIONNDq7ZClZCyWBOnVkROgn4Af2US34mHYme6ExMKdlbm8O87fH0YCIooYBNZBfMGcU2G6IZCpEandTqT4grzSWoJGexMqzZBZAi1jpIenxfnXWegAMe0bQZDZD',
@@ -23,13 +24,6 @@ describe('Facebook Api Integration Tests', () => {
   });
 
   it('Should return undefined if token is invalid', async () => {
-    const axiosClient = new AxiosHttpClient();
-    const sut = new FacebookApi(
-      axiosClient,
-      env.facebookApi.clientId,
-      env.facebookApi.clientSecret,
-    );
-
     const fbUser = await sut.loadUser({ token: 'invalid_token' });
 
     expect(fbUser).toBeUndefined();
