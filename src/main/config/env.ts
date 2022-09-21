c1ce1eb0c52fad2f65147b8d9155f9eb5c563f2f
Allysson <allyssonmgs@gmail.com>
2022-09-20 23:06:16 -0300

    feat: configure express server

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,4 +3,5 @@ export const env = {
     clientId: process.env.FB_CLIENT_ID ?? '1720172065032371',
     clientSecret: process.env.FB_CLIENT_SECRET ?? 'f28c07f1542109e523962395cbce920d',
   },
+  port: process.env.PORT ?? 8080,
 };
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,8 @@
+import './config/module-alias';
+
 import 'reflect-metadata';
+
+import { app } from '@/main/config/app';
+import { env } from '@/main/config/env';
+
+app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`));
