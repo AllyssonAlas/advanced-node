efdda1be884571faef4dc9181ad21fa5a2a274e8
Allysson <allyssonmgs@gmail.com>
2022-09-24 20:23:57 -0300

    test: add facebook login integration tests

--- src/main/routes/login.ts
+++ src/main/routes/login.ts
@@ -4,5 +4,5 @@ import { makeFacebookLoginController } from '@/main/factories/controllers';
 import { adaptExpressRoute as adapt } from '@/infra/http';
 
 export default (router: Router): void => {
-  router.get('/login/facebook', adapt(makeFacebookLoginController()));
+  router.post('/login/facebook', adapt(makeFacebookLoginController()));
 };
---
+++ tests/main/routes/login.spec.ts
@@ -0,0 +1,59 @@
+import request from 'supertest';
+import { IBackup } from 'pg-mem';
+import { getConnection } from 'typeorm';
+
+import { UnauthorizedError } from '@/application/errors';
+import { PgUser } from '@/infra/postgres/entities';
+import { app } from '@/main/config/app';
+
+import { makeFakeDb } from '@/tests/infra/postegres/mocks';
+
+describe('Login Routes', () => {
+  let backup: IBackup;
+  const loadUserSpy = jest.fn();
+
+  jest.mock('@/infra/apis/facebook', () => ({
+    FacebookApi: jest.fn().mockReturnValue({
+      loadUser: loadUserSpy,
+    }),
+  }));
+
+  beforeAll(async () => {
+    const db = await makeFakeDb([PgUser]);
+    backup = db.backup();
+  });
+
+  beforeEach(() => {
+    backup.restore();
+  });
+
+  afterAll(async () => {
+    await getConnection().close();
+  });
+
+  describe('POST /login/facebook', () => {
+    it('Should return 200 with AccessToken', async () => {
+      loadUserSpy.mockResolvedValueOnce({
+        facebookId: 'any_id',
+        name: 'any_name',
+        email: 'any_ email',
+      });
+
+      const { status, body } = await request(app)
+        .post('/api/login/facebook')
+        .send({ token: 'valid_token' });
+
+      expect(status).toBe(200);
+      expect(body.accessToken).toBeDefined();
+    });
+
+    it('Should return 401 with UnauthorizedError', async () => {
+      const { status, body } = await request(app)
+        .post('/api/login/facebook')
+        .send({ token: 'invalid_token' });
+
+      expect(status).toBe(401);
+      expect(body.error).toBe(new UnauthorizedError().message);
+    });
+  });
+});
