d12b2705cfe27ab897d7b4dc2b2d9cc51a024731
Allysson <allyssonmgs@gmail.com>
2022-09-24 15:56:58 -0300

    refactor: transform ExpressRouter to a function

--- src/main/routes/login.ts
+++ src/main/routes/login.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 
+import { makeFacebookLoginController } from '@/main/factories/controllers';
+import { adaptExpressRoute as adapt } from '@/infra/http';
+
 export default (router: Router): void => {
-  router.get('/api/login/facebook', (req, res) => {
-    res.send({ data: 'any_data' });
-  });
+  router.get('/api/login/facebook', adapt(makeFacebookLoginController()));
 };
--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -1,29 +1,31 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { getMockReq, getMockRes } from '@jest-mock/express';
 import { mock, MockProxy } from 'jest-mock-extended';
 
-import { ExpressRouter } from '@/infra/http';
+import { adaptExpressRoute } from '@/infra/http';
 import { Controller } from '@/application/controllers';
 
 describe('ExpressRouter', () => {
   let req: Request;
   let res: Response;
+  let next: NextFunction;
   let controller: MockProxy<Controller>;
-  let sut: ExpressRouter;
+  let sut: RequestHandler;
 
   beforeEach(() => {
     req = getMockReq({ body: { any: 'any' } });
     res = getMockRes().res;
+    next = getMockRes().next;
     controller = mock();
     controller.handle.mockResolvedValue({
       statusCode: 200,
       data: { data: 'any_data' },
     });
-    sut = new ExpressRouter(controller);
+    sut = adaptExpressRoute(controller);
   });
 
   it('Should call handle with correct rquest', async () => {
-    await sut.adapt(req, res);
+    await sut(req, res, next);
 
     expect(controller.handle).toHaveBeenCalledWith({ any: 'any' });
     expect(controller.handle).toHaveBeenCalledTimes(1);
@@ -32,14 +34,14 @@ describe('ExpressRouter', () => {
   it('Should call handle with empty rquest', async () => {
     const req = getMockReq();
 
-    await sut.adapt(req, res);
+    await sut(req, res, next);
 
     expect(controller.handle).toHaveBeenCalledWith({});
     expect(controller.handle).toHaveBeenCalledTimes(1);
   });
 
   it('Should respond with 200 and valid data', async () => {
-    await sut.adapt(req, res);
+    await sut(req, res, next);
 
     expect(res.status).toHaveBeenCalledWith(200);
     expect(res.status).toHaveBeenCalledTimes(1);
@@ -52,7 +54,7 @@ describe('ExpressRouter', () => {
       statusCode: 400,
       data: new Error('any_error'),
     });
-    await sut.adapt(req, res);
+    await sut(req, res, next);
 
     expect(res.status).toHaveBeenCalledWith(400);
     expect(res.status).toHaveBeenCalledTimes(1);
