1c9135f4ca484990cd1ef12e5bb24ccc3b59e02d
Allysson <allyssonmgs@gmail.com>
2022-09-24 20:16:34 -0300

    fix: move getRepository to functions body

--- src/infra/postgres/repositories/account.ts
+++ src/infra/postgres/repositories/account.ts
@@ -14,10 +14,9 @@ type SavaResult = SaveFacebookAccountRepository.Result;
 // eslint-disable-next-line prettier/prettier
 export class PgUserAccountRepository implements LoadUserAccountRepository, SaveFacebookAccountRepository
 {
-  private readonly pgUserRepo = getRepository(PgUser);
-
   async load(params: LoadParams): Promise<LoadResult> {
-    const pgUser = await this.pgUserRepo.findOne(params);
+    const pgUserRepo = getRepository(PgUser);
+    const pgUser = await pgUserRepo.findOne(params);
     if (pgUser) {
       return {
         id: pgUser.id.toString(),
@@ -27,9 +26,10 @@ export class PgUserAccountRepository implements LoadUserAccountRepository, SaveF
   }
 
   async saveWithFacebook(params: SavaParams): Promise<SavaResult> {
+    const pgUserRepo = getRepository(PgUser);
     let id: string;
     if (!params.id) {
-      const pgUser = await this.pgUserRepo.save({
+      const pgUser = await pgUserRepo.save({
         name: params.name,
         email: params.email,
         facebookId: params.facebookId,
@@ -37,7 +37,7 @@ export class PgUserAccountRepository implements LoadUserAccountRepository, SaveF
       id = pgUser.id.toString();
     } else {
       id = params.id;
-      await this.pgUserRepo.update(
+      await pgUserRepo.update(
         { id: parseInt(id) },
         { name: params.name, facebookId: params.facebookId },
       );
