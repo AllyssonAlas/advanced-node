2c75d983ac6b68e78140a064cefde0b47f8d2a17
Allysson <allyssonmgs@gmail.com>
2022-11-13 15:08:59 -0300

    test: ensure JwtTokenHandler calls verify with correct params

--- tests/infra/crypto/jwt-token-handler.spec.ts
+++ tests/infra/crypto/jwt-token-handler.spec.ts
@@ -8,6 +8,7 @@ describe('JwtTokenHandler', () => {
   let sut: JwtTokenHandler;
   let fakeJwt: jest.Mocked<typeof jwt>;
   let secret: string;
+  let token: string;
 
   beforeAll(() => {
     fakeJwt = jwt as jest.Mocked<typeof jwt>;
@@ -21,11 +22,9 @@ describe('JwtTokenHandler', () => {
   describe('generateToken', () => {
     let key: string;
     let expirationInMs: number;
-    let token: string;
 
     beforeAll(() => {
       key = 'any_key';
-      token = 'any_token';
       expirationInMs = 1000;
       fakeJwt.sign.mockImplementation(() => token);
     });
@@ -34,6 +33,7 @@ describe('JwtTokenHandler', () => {
       await sut.generateToken({ key, expirationInMs });
 
       expect(fakeJwt.sign).toHaveBeenCalledWith({ key }, secret, { expiresIn: 1 });
+      expect(fakeJwt.sign).toHaveBeenCalledTimes(1);
     });
 
     it('Should return a token', async () => {
@@ -50,6 +50,15 @@ describe('JwtTokenHandler', () => {
       const promise = sut.generateToken({ key, expirationInMs });
 
       await expect(promise).rejects.toThrow(new Error('token_error'));
+    });
+  });
+
+  describe('validateToken', () => {
+    it('Should call verify with correct params', async () => {
+      await sut.validateToken({ token });
+
+      expect(fakeJwt.verify).toHaveBeenCalledWith(token, secret);
+      expect(fakeJwt.verify).toHaveBeenCalledTimes(1);
     });
   });
 });
