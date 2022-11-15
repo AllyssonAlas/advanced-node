551452f046d5185de4522fc285afbf1bd8933f3e
Allysson <allyssonmgs@gmail.com>
2022-11-15 16:16:13 -0300

    chore: enable debug with ts-node-dev

--- src/main/config/module-alias.ts
+++ src/main/config/module-alias.ts
@@ -1,4 +1,6 @@
 import { addAlias } from 'module-alias';
 import { resolve } from 'path';
 
-addAlias('@', resolve('dist'));
+const devEnvironment = process.env.TS_NODE_DEV === undefined;
+
+addAlias('@', resolve(devEnvironment ? 'dist' : 'src'));
