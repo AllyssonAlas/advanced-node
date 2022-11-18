609175eecc5c5c7964221fb7bec83f0229e89233
Allysson <allyssonmgs@gmail.com>
2022-11-18 17:36:47 -0300

    feat: ensure ChangeProfilePicture calls UploadFile with unique id

--- src/domain/usecases/change-profile-picture.ts
+++ src/domain/usecases/change-profile-picture.ts
@@ -1,20 +1,32 @@
 export namespace UploadFile {
-  export type Params = {
+  export type Input = {
     key: string;
     file: Buffer;
   };
 }
 
 export interface UploadFile {
-  upload: (input: UploadFile.Params) => Promise<void>;
+  upload: (input: UploadFile.Input) => Promise<void>;
+}
+
+export namespace UUIDGenerator {
+  export type Input = {
+    key: string;
+  };
+
+  export type Output = string;
+}
+
+export interface UUIDGenerator {
+  uuid: (input: UUIDGenerator.Input) => UUIDGenerator.Output;
 }
 
 type Input = { id: string; file: Buffer };
 type ChangeProfilePicture = (input: Input) => Promise<void>;
-type Setup = (fileStorage: UploadFile) => ChangeProfilePicture;
+type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator) => ChangeProfilePicture;
 
-export const setupChangeProfilePicture: Setup = (fileStorage) => {
+export const setupChangeProfilePicture: Setup = (fileStorage, crypto) => {
   return async ({ id, file }) => {
-    await fileStorage.upload({ file, key: id });
+    await fileStorage.upload({ file, key: crypto.uuid({ key: id }) });
   };
 };
