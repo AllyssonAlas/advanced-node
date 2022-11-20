d08329d4d9fb32f6342c21c9074e93c2f9a3998b
Allysson <allyssonmgs@gmail.com>
2022-11-20 14:54:32 -0300

    test: ensure ChangeProfilePicture calls SaveUserPicture with correct initials

--- tests/domain/usecases/change-profile-picture.spec.ts
+++ tests/domain/usecases/change-profile-picture.spec.ts
@@ -42,7 +42,10 @@ describe('ChangeProfilePicture', () => {
   it('Should call SaveUserPicture with correct input', async () => {
     await sut({ id: 'any_id', file });
 
-    expect(userProfileRepo.savePicture).toHaveBeenCalledWith({ pictureUrl: 'any_url' });
+    expect(userProfileRepo.savePicture).toHaveBeenCalledWith({
+      pictureUrl: 'any_url',
+      initials: undefined,
+    });
     expect(userProfileRepo.savePicture).toHaveBeenCalledTimes(1);
   });
 
