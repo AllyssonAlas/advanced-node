91b92e61c8d85ec245ad35ce32f7e31fa20fc9c6
Allysson <allyssonmgs@gmail.com>
2022-11-20 18:29:14 -0300

    feat: ensure ChangeProfilePicture calls DeleteFile if file exists and SaveUserPicture throws

--- src/domain/contracts/gateways/file-storage.ts
+++ src/domain/contracts/gateways/file-storage.ts
@@ -1,8 +1,16 @@
+export interface UploadFile {
+  upload: (input: UploadFile.Input) => Promise<UploadFile.Output>;
+}
+
 export namespace UploadFile {
   export type Input = { key: string; file: Buffer };
   export type Output = string;
 }
 
-export interface UploadFile {
-  upload: (input: UploadFile.Input) => Promise<UploadFile.Output>;
+export interface DeleteFile {
+  delete: (input: DeleteFile.Input) => Promise<void>;
+}
+
+export namespace DeleteFile {
+  export type Input = { key: string };
 }
--- src/domain/usecases/change-profile-picture.ts
+++ src/domain/usecases/change-profile-picture.ts
@@ -1,27 +1,32 @@
 import { UserProfile } from '@/domain/entities';
-import { UploadFile, UUIDGenerator } from '@/domain/contracts/gateways';
+import { UploadFile, DeleteFile, UUIDGenerator } from '@/domain/contracts/gateways';
 import { SaveUserPicture, LoadUserProfile } from '@/domain/contracts/repositories';
 
 type Input = { id: string; file?: Buffer };
 type Output = { pictureUrl?: string; name?: string };
 export type ChangeProfilePicture = (input: Input) => Promise<Output>;
 type Setup = (
-  fileStorage: UploadFile,
+  fileStorage: UploadFile & DeleteFile,
   crypto: UUIDGenerator,
   userProfileRepo: SaveUserPicture & LoadUserProfile,
 ) => ChangeProfilePicture;
 
 export const setupChangeProfilePicture: Setup = (fileStorage, crypto, userProfileRepo) => {
   return async ({ id, file }) => {
+    const key = crypto.uuid({ key: id });
     const data: { pictureUrl?: string; name?: string } = {};
     if (file) {
-      data.pictureUrl = await fileStorage.upload({ file, key: crypto.uuid({ key: id }) });
+      data.pictureUrl = await fileStorage.upload({ file, key });
     } else {
       data.name = (await userProfileRepo.load({ id })).name;
     }
     const userProfile = new UserProfile(id);
     userProfile.setPicture(data);
-    await userProfileRepo.savePicture(userProfile);
+    try {
+      await userProfileRepo.savePicture(userProfile);
+    } catch (error) {
+      await fileStorage.delete({ key });
+    }
     return userProfile;
   };
 };
