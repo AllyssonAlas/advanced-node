a3290e0a2eeab07ee2155cfd99be6e6572bb58c6
Allysson <allyssonmgs@gmail.com>
2022-11-23 20:23:31 -0300

    feat: ensure AwsS3FileStorage calls putObject with correct input

--- src/infra/gateways/aws-s3-file-storage.ts
+++ src/infra/gateways/aws-s3-file-storage.ts
@@ -1,12 +1,19 @@
-import { config } from 'aws-sdk';
+import { config, S3 } from 'aws-sdk';
+
+import { UploadFile } from '@/domain/contracts/gateways';
 
 export class AwsS3FileStorage {
-  constructor(private readonly accessKey: string, private readonly secret: string) {
+  constructor(accessKey: string, secret: string, private readonly bucket: string) {
     config.update({
       credentials: {
         accessKeyId: accessKey,
         secretAccessKey: secret,
       },
     });
+  }
+
+  async upload({ key, file }: UploadFile.Input): Promise<void> {
+    const s3 = new S3();
+    await s3.putObject({ Bucket: this.bucket, Key: key, Body: file, ACL: 'public-read' }).promise();
   }
 }
