2ca3a922f4b69d85ab717487046730911c4cd622
Allysson <allyssonmgs@gmail.com>
2022-11-22 21:14:05 -0300

    test: ensure UUIDHandler returns correct value

--- tests/infra/crypto/uuid-handler.spec.ts
+++ tests/infra/crypto/uuid-handler.spec.ts
@@ -1,4 +1,5 @@
 import { v4 } from 'uuid';
+import { mocked } from 'ts-jest/utils';
 
 import { UUIDHandler } from '@/infra/crypto';
 
@@ -11,5 +12,14 @@ describe('UUIDHandler', () => {
     sut.uuid({ key: 'any_key' });
 
     expect(v4).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should return correct uuid', () => {
+    mocked(v4).mockReturnValueOnce('any_uuid');
+    const sut = new UUIDHandler();
+
+    const uuid = sut.uuid({ key: 'any_key' });
+
+    expect(uuid).toBe('any_key_any_uuid');
   });
 });
