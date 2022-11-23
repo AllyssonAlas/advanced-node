760557751c55bc08bf4ea1658b9e326302b6f565
Allysson <allyssonmgs@gmail.com>
2022-11-22 21:35:00 -0300

    test: ensure UniqueId returns correct unique id

---
+++ tests/infra/crypto/unique-id.spec.ts
@@ -0,0 +1,19 @@
+import { UniqueId } from '@/infra/crypto';
+
+describe('UniqueId', () => {
+  it('Should return an unique id', () => {
+    const sut = new UniqueId(new Date(2021, 9, 3, 10, 10, 10));
+
+    const uuid = sut.uuid({ key: 'any_key' });
+
+    expect(uuid).toBe('any_key_20211003101010');
+  });
+
+  it('Should return another unique id', () => {
+    const sut = new UniqueId(new Date(2018, 2, 10, 18, 1, 0));
+
+    const uuid = sut.uuid({ key: 'any_key' });
+
+    expect(uuid).toBe('any_key_20180310180100');
+  });
+});
