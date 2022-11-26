d31d572d7de7961260fd5dcacb9f2721a2fccfc9
Allysson <allyssonmgs@gmail.com>
2022-11-26 12:22:48 -0300

    test: ensure FacebookLoginController extends Controller

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -1,5 +1,5 @@
 import { AuthenticationError } from '@/domain/entities/errors';
-import { FacebookLoginController } from '@/application/controllers';
+import { Controller, FacebookLoginController } from '@/application/controllers';
 import { UnauthorizedError } from '@/application/errors';
 import { RequiredStringValidator } from '@/application/validation';
 
@@ -16,6 +16,10 @@ describe('FacebookLoginController', () => {
 
   beforeEach(() => {
     sut = new FacebookLoginController(facebookAuth);
+  });
+
+  it('Should extend controller', () => {
+    expect(sut).toBeInstanceOf(Controller);
   });
 
   it('Should build Validators correctly', () => {
