f7e1c41bd2a2629f29fdb2c9516d987117b0d80b
Allysson <allyssonmgs@gmail.com>
2022-11-26 13:03:45 -0300

    refactor: isolate sut creation

--- tests/application/controllers/save-picture.spec.ts
+++ tests/application/controllers/save-picture.spec.ts
@@ -2,10 +2,14 @@ import { SavePictureController } from '@/application/controllers';
 import { RequiredFieldError } from '@/application/errors';
 
 describe('SavePictureController', () => {
-  it('Shoud return 400 if file is not provided', async () => {
-    const sut = new SavePictureController();
+  let sut: SavePictureController;
 
-    const httpResponse = await sut.handle({ file: undefined });
+  beforeEach(() => {
+    sut = new SavePictureController();
+  });
+
+  it('Shoud return 400 if file is not provided', async () => {
+    const httpResponse = await sut.handle({ file: undefined as any });
 
     expect(httpResponse).toEqual({ statusCode: 400, data: new RequiredFieldError('file') });
   });
