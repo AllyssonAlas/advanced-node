9d19ac11a5d7d0adff00fa35d9e8af7431db9b92
Allysson <allyssonmgs@gmail.com>
2022-11-26 13:26:48 -0300

    feat: ensure SavePictureController accepts valid mime types

--- src/application/controllers/save-picture.ts
+++ src/application/controllers/save-picture.ts
@@ -5,8 +5,10 @@ type HttpRequest = { file: { buffer: Buffer; mimeType: string } };
 type Model = Error;
 
 export class SavePictureController {
-  async handle({ file }: HttpRequest): Promise<HttpResponse<Model>> {
+  async handle({ file }: HttpRequest): Promise<HttpResponse<Model> | undefined> {
     if (!file || !file?.buffer?.length) return badRequest(new RequiredFieldError('file'));
-    return badRequest(new InvalidMimeTypeError(['png', 'jpeg']));
+    if (!['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimeType)) {
+      return badRequest(new InvalidMimeTypeError(['png', 'jpeg']));
+    }
   }
 }
