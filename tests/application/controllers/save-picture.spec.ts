cc652a31552584e0c5e959f109a738f0550a6112
Allysson <allyssonmgs@gmail.com>
2022-11-26 13:04:47 -0300

    test: ensure SavePictureController return 400 if file is empty

--- tests/application/controllers/save-picture.spec.ts
+++ tests/application/controllers/save-picture.spec.ts
@@ -13,4 +13,16 @@ describe('SavePictureController', () => {
 
     expect(httpResponse).toEqual({ statusCode: 400, data: new RequiredFieldError('file') });
   });
+
+  it('Shoud return 400 if file is null', async () => {
+    const httpResponse = await sut.handle({ file: null as any });
+
+    expect(httpResponse).toEqual({ statusCode: 400, data: new RequiredFieldError('file') });
+  });
+
+  it('Shoud return 400 if file is empty', async () => {
+    const httpResponse = await sut.handle({ file: { buffer: Buffer.from('') } });
+
+    expect(httpResponse).toEqual({ statusCode: 400, data: new RequiredFieldError('file') });
+  });
 });
