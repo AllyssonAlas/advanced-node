e882290cd803114d6add44f66ff78869d0039cfa
Allysson <allyssonmgs@gmail.com>
2022-11-26 12:34:29 -0300

    refactor: isolate sut creation

--- tests/application/controllers/delete-profile-picture.spec.ts
+++ tests/application/controllers/delete-profile-picture.spec.ts
@@ -1,10 +1,18 @@
 import { DeletePictureController } from '@/application/controllers';
 
 describe('DeletePictureController', () => {
-  it('Should call ChangeProfilePicture with correct input', async () => {
-    const changeProfilePicture = jest.fn();
-    const sut = new DeletePictureController(changeProfilePicture);
+  let sut: DeletePictureController;
+  let changeProfilePicture: jest.Mock;
 
+  beforeAll(() => {
+    changeProfilePicture = jest.fn();
+  });
+
+  beforeEach(() => {
+    sut = new DeletePictureController(changeProfilePicture);
+  });
+
+  it('Should call ChangeProfilePicture with correct input', async () => {
     await sut.handle({ userId: 'any_user_id' });
 
     expect(changeProfilePicture).toHaveBeenCalledWith({ id: 'any_user_id' });
