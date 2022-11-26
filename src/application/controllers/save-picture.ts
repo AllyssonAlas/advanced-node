25ec64f9ced982e513527933b7db1eefe4f3c140
Allysson <allyssonmgs@gmail.com>
2022-11-26 13:49:30 -0300

    feat: ensure SavePictureController calls ChangeProfilePicture with correct input

--- src/application/controllers/save-picture.ts
+++ src/application/controllers/save-picture.ts
@@ -1,15 +1,19 @@
+import { ChangeProfilePicture } from '@/domain/usecases';
 import { HttpResponse, badRequest } from '@/application/helpers';
 import { InvalidMimeTypeError, RequiredFieldError, MaxFileSizeError } from '@/application/errors';
 
-type HttpRequest = { file: { buffer: Buffer; mimeType: string } };
+type HttpRequest = { file: { buffer: Buffer; mimeType: string }; userId: string };
 type Model = Error;
 
 export class SavePictureController {
-  async handle({ file }: HttpRequest): Promise<HttpResponse<Model> | undefined> {
+  constructor(private readonly changeProfilePicture: ChangeProfilePicture) {}
+
+  async handle({ file, userId }: HttpRequest): Promise<HttpResponse<Model> | undefined> {
     if (!file || !file?.buffer?.length) return badRequest(new RequiredFieldError('file'));
     if (!['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimeType)) {
       return badRequest(new InvalidMimeTypeError(['png', 'jpeg']));
     }
     if (file.buffer.length > 5 * 1024 * 1024) return badRequest(new MaxFileSizeError(5));
+    await this.changeProfilePicture({ id: userId, file: file.buffer });
   }
 }
