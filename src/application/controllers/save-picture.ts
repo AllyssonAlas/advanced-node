f4fa5246e39de9616feca7655ef178b98f05ddab
Allysson <allyssonmgs@gmail.com>
2022-11-26 14:00:20 -0300

    feat: ensure SavePictureController returns 200 with correct data

--- src/application/controllers/save-picture.ts
+++ src/application/controllers/save-picture.ts
@@ -1,19 +1,20 @@
 import { ChangeProfilePicture } from '@/domain/usecases';
-import { HttpResponse, badRequest } from '@/application/helpers';
+import { HttpResponse, badRequest, ok } from '@/application/helpers';
 import { InvalidMimeTypeError, RequiredFieldError, MaxFileSizeError } from '@/application/errors';
 
 type HttpRequest = { file: { buffer: Buffer; mimeType: string }; userId: string };
-type Model = Error;
+type Model = Error | { initials?: string; pictureUrl?: string };
 
 export class SavePictureController {
   constructor(private readonly changeProfilePicture: ChangeProfilePicture) {}
 
-  async handle({ file, userId }: HttpRequest): Promise<HttpResponse<Model> | undefined> {
+  async handle({ file, userId }: HttpRequest): Promise<HttpResponse<Model>> {
     if (!file || !file?.buffer?.length) return badRequest(new RequiredFieldError('file'));
     if (!['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimeType)) {
       return badRequest(new InvalidMimeTypeError(['png', 'jpeg']));
     }
     if (file.buffer.length > 5 * 1024 * 1024) return badRequest(new MaxFileSizeError(5));
-    await this.changeProfilePicture({ id: userId, file: file.buffer });
+    const data = await this.changeProfilePicture({ id: userId, file: file.buffer });
+    return ok(data);
   }
 }
