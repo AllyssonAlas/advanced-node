4ccc929fa4c830ceafe48504a0c5884e3ff98046
Allysson <allyssonmgs@gmail.com>
2022-11-27 13:06:16 -0300

    feat: ensure AllowedMimeTypes returns undefined if value is valid

--- src/application/validation/allowed-mime-types.ts
+++ src/application/validation/allowed-mime-types.ts
@@ -3,9 +3,20 @@ import { InvalidMimeTypeError } from '@/application/errors';
 type Extension = 'png' | 'jpg';
 
 export class AllowedMimeTypes {
-  constructor(private readonly allowed: Extension[], private readonly mimeType?: string) {}
+  constructor(private readonly allowed: Extension[], private readonly mimeType: string) {}
 
-  validate(): Error {
-    return new InvalidMimeTypeError(this.allowed);
+  validate(): Error | undefined {
+    let isValid = false;
+    if (this.isPng()) isValid = true;
+    else if (this.isJpg()) isValid = true;
+    if (!isValid) return new InvalidMimeTypeError(this.allowed);
+  }
+
+  private isPng(): boolean {
+    return this.allowed.includes('png') && this.mimeType === 'image/png';
+  }
+
+  private isJpg(): boolean {
+    return this.allowed.includes('jpg') && /image\/jpe?g/.test(this.mimeType);
   }
 }
