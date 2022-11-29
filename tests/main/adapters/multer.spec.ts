fd845329e0a2f497ffa59f12cccd495a583606a0
Allysson <allyssonmgs@gmail.com>
2022-11-29 20:25:34 -0300

    refactor: isolate sut creation

--- tests/main/adapters/multer.spec.ts
+++ tests/main/adapters/multer.spec.ts
@@ -1,6 +1,7 @@
 import multer from 'multer';
 
 import { mocked } from 'ts-jest/utils';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { getMockReq, getMockRes } from '@jest-mock/express';
 
 import { adaptMulter } from '@/main/adapters';
@@ -8,17 +9,31 @@ import { adaptMulter } from '@/main/adapters';
 jest.mock('multer');
 
 describe('MulterAdapter', () => {
-  it('Should call single upload with correct input', () => {
-    const uploadSpy = jest.fn();
-    const singleSpy = jest.fn().mockImplementation(() => uploadSpy);
-    const multerSpy = jest.fn().mockImplementation(() => ({ single: singleSpy }));
-    const fakeMulter = multer as jest.Mocked<typeof multer>;
-    mocked(fakeMulter).mockImplementation(multerSpy);
-    const req = getMockReq({ body: { anyBody: 'any_body' }, locals: { anyLocals: 'any_locals' } });
-    const res = getMockRes().res;
-    const next = getMockRes().next;
-    const sut = adaptMulter;
+  let uploadSpy: jest.Mock;
+  let singleSpy: jest.Mock;
+  let multerSpy: jest.Mock;
+  let fakeMulter: jest.Mocked<typeof multer>;
+  let req: Request;
+  let res: Response;
+  let next: NextFunction;
+  let sut: RequestHandler;
 
+  beforeAll(() => {
+    uploadSpy = jest.fn();
+    singleSpy = jest.fn().mockImplementation(() => uploadSpy);
+    multerSpy = jest.fn().mockImplementation(() => ({ single: singleSpy }));
+    fakeMulter = multer as jest.Mocked<typeof multer>;
+    mocked(fakeMulter).mockImplementation(multerSpy);
+    req = getMockReq({ body: { anyBody: 'any_body' }, locals: { anyLocals: 'any_locals' } });
+    res = getMockRes().res;
+    next = getMockRes().next;
+  });
+
+  beforeEach(() => {
+    sut = adaptMulter;
+  });
+
+  it('Should call single upload with correct input', () => {
     sut(req, res, next);
 
     expect(multerSpy).toHaveBeenCalledWith();
