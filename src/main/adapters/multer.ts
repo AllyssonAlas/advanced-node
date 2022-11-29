20a4f61f67462b93c0cb7db61bf1b86cd78ae42d
Allysson <allyssonmgs@gmail.com>
2022-11-29 20:31:51 -0300

    feat: ensure MulterAdapter returns 500 if upload fails

--- src/main/adapters/multer.ts
+++ src/main/adapters/multer.ts
@@ -1,9 +1,10 @@
+import { ServerError } from '@/application/errors';
 import { RequestHandler } from 'express';
 import multer from 'multer';
 
 export const adaptMulter: RequestHandler = (req, res, next) => {
   const upload = multer().single('picture');
-  upload(req, res, () => {
-    next();
+  upload(req, res, (error) => {
+    res.status(500).json({ error: new ServerError(error).message });
   });
 };
