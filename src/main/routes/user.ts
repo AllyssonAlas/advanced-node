131650320582d6e1a65b071065d8ab3836cd72dc
Allysson <allyssonmgs@gmail.com>
2022-11-30 07:35:08 -0300

    feat: ensure PUT  /users/picture returns 403 if no authorization header is present

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -6,4 +6,5 @@ import { auth } from '@/main/middlewares';
 
 export default (router: Router): void => {
   router.delete('/users/picture', auth, adapt(makeSavePictureController()));
+  router.put('/users/picture', auth);
 };
