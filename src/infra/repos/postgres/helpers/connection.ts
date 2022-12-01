7c41ad2983ad3f45d4e79be8b8add1cf887afac6
Allysson <allyssonmgs@gmail.com>
2022-12-01 19:30:35 -0300

    feat: ensure PgConnection is a singleton

---
+++ src/infra/repos/postgres/helpers/connection.ts
@@ -0,0 +1,12 @@
+export class PgConnection {
+  private static instance?: PgConnection;
+
+  private constructor() {}
+
+  static getInstance(): PgConnection {
+    if (PgConnection.instance === undefined) {
+      PgConnection.instance = new PgConnection();
+    }
+    return PgConnection.instance;
+  }
+}
