7f4f27b773629f26a726651d5ac28ec51ddbb89a
Allysson <allyssonmgs@gmail.com>
2022-12-03 20:48:11 -0300

    chore: move typeorm connection options fro .env to ormconfig.js

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,15 +2,12 @@ import './config/module-alias';
 
 import 'reflect-metadata';
 
-import { env } from '@/main/config/env';
-import { createConnection, getConnectionOptions } from 'typeorm';
+import { createConnection } from 'typeorm';
 
-getConnectionOptions()
-  .then(async (options) => {
-    const devEnvironment = process.env.TS_NODE_DEV === undefined;
-    const root = devEnvironment ? 'src' : 'dist';
-    const entities = [`${root}/infra/postgres/entities/index.{js,ts}`];
-    await createConnection({ ...options, entities });
+import { env } from '@/main/config/env';
+
+createConnection()
+  .then(async () => {
     const { app } = await import('@/main/config/app');
     app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`));
   })
