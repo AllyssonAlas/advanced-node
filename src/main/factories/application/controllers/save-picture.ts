b316b8c256495a524a12440617facafb5efb6e62
Allysson <allyssonmgs@gmail.com>
2022-12-04 18:09:24 -0300

    feat: add PgTransactionControllerFactory

--- src/main/factories/application/controllers/save-picture.ts
+++ src/main/factories/application/controllers/save-picture.ts
@@ -1,6 +1,8 @@
-import { SavePictureController } from '@/application/controllers';
+import { SavePictureController, Controller } from '@/application/controllers';
 import { makeChangeProfilePicture } from '@/main/factories/domain/usecases';
+import { makePgTransactionController } from '@/main/factories/application/decorators';
 
-export const makeSavePictureController = (): SavePictureController => {
-  return new SavePictureController(makeChangeProfilePicture());
+export const makeSavePictureController = (): Controller => {
+  const controller = new SavePictureController(makeChangeProfilePicture());
+  return makePgTransactionController(controller);
 };
