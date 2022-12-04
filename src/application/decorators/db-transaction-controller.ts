a05f452ed1e860365b990b0960fd7f91a8686541
Allysson <allyssonmgs@gmail.com>
2022-12-04 17:23:55 -0300

    feat: ensure DbTransactionController returns same result as decoratee as success

--- src/application/decorators/db-transaction-controller.ts
+++ src/application/decorators/db-transaction-controller.ts
@@ -1,15 +1,17 @@
 import { Controller } from '@/application/controllers';
 import { DbTransaction } from '@/application/contracts';
+import { HttpResponse } from '@/application/helpers';
 
 export class DbTransactionController {
   constructor(private readonly decoratee: Controller, private readonly db: DbTransaction) {}
 
-  async perform(httpRequest: any): Promise<void> {
+  async perform(httpRequest: any): Promise<HttpResponse | undefined> {
     await this.db.openTransaction();
     try {
-      await this.decoratee.perform(httpRequest);
+      const httpResponse = await this.decoratee.perform(httpRequest);
       await this.db.commit();
-    } catch (error) {
+      return httpResponse;
+    } catch {
       await this.db.rollback();
     } finally {
       await this.db.closeTransaction();
